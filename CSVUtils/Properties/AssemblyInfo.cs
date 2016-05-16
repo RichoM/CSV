0bd69447a91721e20cc1bebf2130a812512bdd94
RichoM <richi.moran@gmail.com>
2016-05-16 16:10:43 -0300

    Small changes in the AssemblyInfo to see how they impact on the nuget package.

--- CSVUtils/Properties/AssemblyInfo.cs
+++ CSVUtils/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("CSV")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A CSV parser")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Ricardo Moran")]
 [assembly: AssemblyProduct("CSV")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
