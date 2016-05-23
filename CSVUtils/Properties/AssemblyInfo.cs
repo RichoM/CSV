9733760ed69f5ffd7306507309c98c02b2bbaf57
RichoM <richi.moran@gmail.com>
2016-05-23 11:37:07 -0300

    Changed csproj and AssemblyInfo

--- CSVUtils/Properties/AssemblyInfo.cs
+++ CSVUtils/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("CSV")]
+[assembly: AssemblyTitle("RichoM.CSV")]
 [assembly: AssemblyDescription("A CSV parser")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Ricardo Moran")]
-[assembly: AssemblyProduct("CSV")]
+[assembly: AssemblyProduct("RichoM.CSV")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
